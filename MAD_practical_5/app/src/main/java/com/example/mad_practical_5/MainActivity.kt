0a23ee344094a63a0f652a91dbf17662bdc21f2a
Aashish Shah <aashishsah005@gmail.com>
2025-11-17 13:35:35 +0530

    comitted

--- MAD_practical_5/app/src/main/java/com/example/mad_practical_5/MainActivity.kt
+++ MAD_practical_5/app/src/main/java/com/example/mad_practical_5/MainActivity.kt
@@ -5,43 +5,43 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.mad_practical_5.ui.theme.MAD_practical_5Theme
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.mad_practical_5.ui.theme.JetPackTheme
+import com.example.mad_practical_5.screen.LoginUI
+import com.example.mad_practical_5.screen.RegisterUI
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
-            MAD_practical_5Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            JetPackTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+
+                    NavHost(
+                        navController = navController,
+                        startDestination = "LoginScreen"
+                    ) {
+                        composable("Register") {
+                            RegisterUI(navController)
+                        }
+                        composable("LoginScreen") {
+                            LoginUI(navController)
+                        }
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MAD_practical_5Theme {
-        Greeting("Android")
-    }
-}
